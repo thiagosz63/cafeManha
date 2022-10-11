fd07f844f0fd85cc75e83010af66dad304e1113a
thiagosz63 <thiagosz63@gmail.com>
2022-10-10 22:25:17 -0300

    Validando CPF já cadastrado

--- backend/src/main/java/com/fcb/cafeDaManha/service/validation/ColaboradorInsertValidator.java
+++ backend/src/main/java/com/fcb/cafeDaManha/service/validation/ColaboradorInsertValidator.java
@@ -6,17 +6,29 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.fcb.cafeDaManha.controller.exception.FieldMessage;
 import com.fcb.cafeDaManha.dto.ColaboradorNewDTO;
+import com.fcb.cafeDaManha.entities.Colaborador;
+import com.fcb.cafeDaManha.repository.ColaboradorRepository;
 import com.fcb.cafeDaManha.service.validation.utils.BR;
 
 public class ColaboradorInsertValidator implements ConstraintValidator<ColaboradorInsert, ColaboradorNewDTO> {
+
+	@Autowired
+	private ColaboradorRepository colaboradorRepository;
 
 	@Override
 	public boolean isValid(ColaboradorNewDTO objDTO, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 		if (!BR.isValidCPF(objDTO.getCpf())) {
 			list.add(new FieldMessage("cpf", "CPF inválido"));
+		}
+		
+		Colaborador col = colaboradorRepository.buscarPorCpf(objDTO.getCpf());
+		if (col != null) {
+			list.add(new FieldMessage("cpf", "CPF já cadastrado"));
 		}
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
